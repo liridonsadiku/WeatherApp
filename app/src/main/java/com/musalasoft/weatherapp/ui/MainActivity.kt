a8a6b07c9161de4e28db6119f75fdcf4f6404fc0
liridonsadiku <liridonsadikuu@gmail.com>
2021-03-09 15:12:47 +0100

    Handle location permission and get current location coordinates: latitude and longitude

--- app/src/main/java/com/musalasoft/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/musalasoft/weatherapp/ui/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.musalasoft.weatherapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
-import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.*
 import com.musalasoft.weatherapp.R
 import com.musalasoft.weatherapp.databinding.ActivityMainBinding
 import com.musalasoft.weatherapp.viewmodel.HomeViewModel
+import pub.devrel.easypermissions.AppSettingsDialog
 import pub.devrel.easypermissions.EasyPermissions
+
 
 class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
     lateinit var viewModel: HomeViewModel
@@ -22,15 +28,81 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         binding.viewModel = viewModel
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        getLastLocation()
+    }
 
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+
+        if (EasyPermissions.hasPermissions(this, Manifest.permission.ACCESS_FINE_LOCATION,
+                                                         Manifest.permission.ACCESS_COARSE_LOCATION,
+                                                         Manifest.permission.INTERNET)) {
+                observeViewModel()
+                mFusedLocationClient.lastLocation.addOnCompleteListener { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        binding.tvCurrentLocation.text = location.latitude.toString() +" njeshi " + location.longitude.toString()
+                    }
+
+                }
+        } else {
+            EasyPermissions.requestPermissions(this, getString(R.string.enable_location),
+                    RC_SMS_PERM, Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.INTERNET);
+        }
+    }
+
+    private fun observeViewModel() {
 
     }
 
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+            binding.tvCurrentLocation.text = mLastLocation.latitude.toString() +" dyshi " + mLastLocation.longitude.toString()
+
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 5
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper()
+        )
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+
+    }
     override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        }else{
+            EasyPermissions.requestPermissions(this, getString(R.string.enable_location),
+                    RC_SMS_PERM,Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.INTERNET);
+        }
 
     }
 
     override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
-
+        getLastLocation()
     }
+
 }
